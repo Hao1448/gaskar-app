297d9ade9a8cb182872b4483414c771b93f1625d
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-02 23:21:57 +0300

    update widget

--- src/components/WidgetProjects/index.js
+++ src/components/WidgetProjects/index.js
@@ -1,23 +1,56 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import ReactModal from 'react-modal';
 import { UiButton } from '../Ui'
 import { CardProject } from '../CardProject'
-
+import  { addProject } from '../../_actions'
 import './WidgetProjects.scss'
 
-export const WidgetProjects = () => {
-    return (
-        <div className="widgetProjects">
-            <div className="widgetProjects-top">
-                <div className="widgetProjects-title">список проектов</div>
-                <div className="widgetProjects-button">
-                    <UiButton value="Добавить проект"/>
+class WidgetProjects extends React.Component {
+    constructor () {
+        super();
+            this.state = {
+            showModal: false
+        };
+      
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+    
+    handleOpenModal () {
+        this.setState({ showModal: true });
+    }
+    
+    handleCloseModal () {
+      this.setState({ showModal: false });
+    }
+    
+    render () {
+        console.log(this.props)
+        return (
+            <div className="widgetProjects">
+                <div className="widgetProjects-top">
+                    <div className="widgetProjects-title">список проектов</div>
+                    <div className="widgetProjects-button">
+                        <UiButton onClick={this.handleOpenModal} value="Добавить проект"/>
+                        <ReactModal 
+                            isOpen={this.state.showModal}
+                            contentLabel="Minimal Modal Example"
+                        >
+                        <button onClick={this.handleCloseModal}>Close Modal</button>
+                </ReactModal>
+                    </div>
+                </div>
+                <div className="widgetProjects-cards">
+                    <div className="widgetProjects-cards_card">
+                        <CardProject/>
+                    </div>
                 </div>
             </div>
-            <div className="widgetProjects-cards">
-                <div className="widgetProjects-cards_card">
-                    <CardProject/>
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default connect(state => ({
+    projects: state.projects
+}), { addProject })(WidgetProjects)
