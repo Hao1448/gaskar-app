21778f467242da7c3e1c6766c490e4cbea7bd315
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-02 23:20:47 +0300

    Create projects.actions.js

---
+++ src/_actions/projects.actions.js
@@ -0,0 +1,9 @@
+import { ADD_PROJECT } from '../_constants'
+
+export const addProject = (name, duration, leader, admin) => ({
+    type: ADD_PROJECT,
+    name,
+    duration,
+    leader,
+    admin
+});
