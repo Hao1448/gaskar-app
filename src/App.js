f5dbf9a1249a7828db814084736e085310b0bc29
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-02 23:20:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route, Router } from 'react-router-dom'
-import { Home } from './pages/Home'
-import HomePage from './pages/HomePage'
+import React, { Fragment } from 'react'
+import { Route, Router } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
 import { Projects } from './pages/Projects'
 import { connect } from 'react-redux';
@@ -22,33 +20,28 @@ class App extends React.Component {
   }
 
   render() {
-      const { alert } = this.props;
-      return (
-          <div className="jumbotron">
-              <div className="container">
-                  <div className="col-sm-8 col-sm-offset-2">
-                      {alert.message &&
-                          <div className={`alert ${alert.type}`}>{alert.message}</div>
-                      }
-                      <Router history={history}>
-                          <div>
-                              <PrivateRoute exact path="/" component={HomePage} />
-                              <PrivateRoute component={Projects} path="/projects" />
-                              <Route component={LoginPage} path="/login" exact/>
-                          </div>
-                      </Router>
-                  </div>
-              </div>
-          </div>
-      );
-  }
+    const { alert } = this.props;
+        return (
+            <Fragment>
+                {alert.message &&
+                    <div className={`alert ${alert.type}`}>{alert.message}</div>
+                }
+                <Router history={history}>
+                    <div>
+                        <Route exact path="/" component={LoginPage} />
+                        <PrivateRoute component={Projects} path="/projects" />
+                    </div>
+                </Router>
+            </Fragment>
+        );
+    }
 }
 
 function mapStateToProps(state) {
-  const { alert } = state;
-  return {
-      alert
-  };
+    const { alert } = state;
+    return {
+        alert
+    };
 }
 
 const connectedApp = connect(mapStateToProps)(App);
