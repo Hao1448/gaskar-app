5d777f2b8b310cf2aad5510ded83f5f9afee18a8
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-02 17:38:57 +0300

    add fake-auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Router } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { LoginPage } from './pages/LoginPage'
 import { Projects } from './pages/Projects'
+import { connect } from 'react-redux';
+import { history } from './_helpers';
+import { alertActions } from './_actions';
+import { PrivateRoute } from './_components';
+import './index.scss'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route component={Home} path="/" exact />
-        <Route component={Projects} path="/projects" />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+
+      const { dispatch } = this.props;
+      history.listen((location, action) => {
+          // clear alert on location change
+          dispatch(alertActions.clear());
+      });
+  }
+
+  render() {
+      const { alert } = this.props;
+      return (
+          <div className="jumbotron">
+              <div className="container">
+                  <div className="col-sm-8 col-sm-offset-2">
+                      {alert.message &&
+                          <div className={`alert ${alert.type}`}>{alert.message}</div>
+                      }
+                      <Router history={history}>
+                          <div>
+                              <PrivateRoute exact path="/" component={Home} />
+                              <Route path="/login" component={LoginPage} />
+                              <Route component={Projects} path="/projects" />
+                              <Route component={LoginPage} path="/login" exact/>
+                          </div>
+                      </Router>
+                  </div>
+              </div>
+          </div>
+      );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  const { alert } = state;
+  return {
+      alert
+  };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+
+export default connectedApp ; 
