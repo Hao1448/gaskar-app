bc7246596994c10a634e30636a1b31651aa12003
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-03 16:13:08 +0300

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -44,26 +44,27 @@ class LoginPage extends React.Component {
         const { loggingIn } = this.props;
         const { username, password, submitted } = this.state;
         return (
-            <div className="home">
-                <div className="col">
+            <div className="login">
+                <div className="login-col">
                     <form name="form" onSubmit={this.handleSubmit}>
-                       <div className="home_input">
+                       <div className="login-input">
                             <UiInput type="text" name="username" placeholder="Логин" value={username} onChange={this.handleChange}/>
                             {submitted && !username &&
-                                <div className="help-block">Необходим логин</div>
+                                <div>Необходим логин</div>
                             }
                         </div>
-                        <div className="home_input">
+                        <div className="login-input">
                             <UiInput type="password" name="password" placeholder="Пароль" value={password} onChange={this.handleChange}/>
                             {submitted && !password &&
-                                <div className="help-block">Необходим пароль</div>
+                                <div>Необходим пароль</div>
                             }
                         </div>
-                        <UiButton value="Войти"/>
-                        <div>
+                        <div className="login-checkbox">
                             <input type="checkbox" id="scales" name="scales"/>
+                            <span></span>
                             <label forHtml="scales">Запомнить пароль</label>
                         </div>
+                        <UiButton value="Войти"/>
                         <div>Забыли пароль?</div>
                         {loggingIn &&
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
