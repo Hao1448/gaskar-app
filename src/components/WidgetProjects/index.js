57db4a0343ca1746c178300236e938f308b9fd65
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-04 17:38:57 +0300

    Update WidgetProjects

--- src/components/WidgetProjects/index.js
+++ src/components/WidgetProjects/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import ReactModal from 'react-modal';
+import Modal from 'react-modal';
 import { UiButton } from '../Ui'
 import { CardProject } from '../CardProject'
+import { UiInput } from '../Ui'
 import  { addProject } from '../../_actions'
 import './WidgetProjects.scss'
 
@@ -15,6 +16,7 @@ class WidgetProjects extends React.Component {
       
         this.handleOpenModal = this.handleOpenModal.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
+        this.submitHandler = this.submitHandler.bind(this);
     }
     
     handleOpenModal () {
@@ -24,28 +26,65 @@ class WidgetProjects extends React.Component {
     handleCloseModal () {
       this.setState({ showModal: false });
     }
-    
+
+    submitHandler (event) {
+        event.preventDefault()
+        const newProject = {
+            name,
+            duration,
+            leader,
+            admin
+        }
+        this.props.addProject({newProject})
+    }
+
     render () {
-        console.log(this.props)
+        const { projects } = this.props;
+
+        const customStyles = {
+            overlay: {
+                display: 'grid',
+                gridTemplateColumns: 'repeat(12, 1fr)',
+                gridGap: '20px'
+            },
+            content : {
+                position: 'relative',
+                gridColumn: '4 / span 5',
+                alignSelf: 'center'
+            }
+          }
+          console.log(this.props)
         return (
+         
             <div className="widgetProjects">
                 <div className="widgetProjects-top">
                     <div className="widgetProjects-title">список проектов</div>
                     <div className="widgetProjects-button">
                         <UiButton onClick={this.handleOpenModal} value="Добавить проект"/>
-                        <ReactModal 
+                        <Modal 
                             isOpen={this.state.showModal}
-                            contentLabel="Minimal Modal Example"
+                            style={customStyles}
                         >
-                        <button onClick={this.handleCloseModal}>Close Modal</button>
-                </ReactModal>
+                            <button onClick={this.handleCloseModal}>Закрыть</button>
+                            <form onSubmit={this.submitHandler} className="widget-modal">
+                                Добавить проект
+                                <UiInput placeholder="Название проекта"/>
+                                <UiInput placeholder="Даты выполнения"/>
+                                <UiInput placeholder="Руководитель проекта"/>
+                                <UiInput placeholder="Администратор проекта"/>
+                                <UiButton type="submit" value="Создать проект"/>
+                            </form>
+                        </Modal>
                     </div>
                 </div>
-                <div className="widgetProjects-cards">
-                    <div className="widgetProjects-cards_card">
-                        <CardProject/>
+                { projects.map((project) => 
+                    <div className="widgetProjects-cards">
+                        <div className="widgetProjects-cards_card">
+                            <CardProject project={project}/>
+                        </div>
                     </div>
-                </div>
+                    )
+                }
             </div>
         )
     }
