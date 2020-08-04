380f84a64ec2f62c66c395ab439047c5ca7cec8d
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-04 17:39:07 +0300

    Update CardProject

--- src/components/CardProject/index.js
+++ src/components/CardProject/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './CardProject.scss'
 
-export const CardProject = () => {
+export const CardProject = ({project}) => {
     return (
         <div className="card">
             <div className="grid">
@@ -13,10 +13,10 @@ export const CardProject = () => {
                     <div className="top-title">Hedassah Medical</div>
                 </div>
                 <div className="bottom">
-                    <div className="bottom-title">Терапевтический корпус</div>
-                    <div className="bottom-row">08.04.2019-31.01.21 гг.</div>
-                    <div className="bottom-row">Денис Конев</div>
-                    <div className="bottom-row">Гросолим Лимитед</div>
+                    <div className="bottom-title">{project.name}</div>
+                    <div className="bottom-row">{project.duration}</div>
+                    <div className="bottom-row">{project.leader}</div>
+                    <div className="bottom-row">{project.admin}</div>
                 </div>
             </div>
         </div>
