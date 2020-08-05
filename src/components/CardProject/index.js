4c21225d5266ebf0639537f84dbae2ae7d262a7e
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-05 17:56:24 +0300

    Update CardProject

--- src/components/CardProject/index.js
+++ src/components/CardProject/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ProfileImage } from '../ProfileImage'
 import './CardProject.scss'
 
 export const CardProject = ({project}) => {
@@ -13,10 +14,29 @@ export const CardProject = ({project}) => {
                     <div className="top-title">Hedassah Medical</div>
                 </div>
                 <div className="bottom">
-                    <div className="bottom-title">{project.name}</div>
-                    <div className="bottom-row">{project.duration}</div>
-                    <div className="bottom-row">{project.leader}</div>
-                    <div className="bottom-row">{project.admin}</div>
+                    <div className="bottom-row">
+                        <div className="bottom-title">{project.name}</div>
+                        <div className="bottom-item">{project.duration}</div>
+                        <div className="bottom-item">{project.leader}</div>
+                        <div className="bottom-item">{project.admin}</div>
+                    </div>
+                    <div className="bottom-row bottom-flex">
+                        <div className="bottom-persons">
+                            <div className="bottom-person">
+                                <ProfileImage/>
+                            </div>
+                            <div className="bottom-person">
+                                <ProfileImage/>
+                            </div>
+                            <div className="bottom-person">
+                                <ProfileImage/>
+                            </div>
+                            <div className="bottom-person">
+                                <ProfileImage/>
+                            </div>
+                        </div>
+                        <div className="bottom-round"/>
+                    </div>
                 </div>
             </div>
         </div>
