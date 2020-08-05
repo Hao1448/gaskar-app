1aa24f84784c9ed12409baed06f5d336f3e3bfcf
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-05 14:00:33 +0300

    Update projects.actions.js

--- src/_actions/projects.actions.js
+++ src/_actions/projects.actions.js
@@ -1,6 +1,6 @@
 import { ADD_PROJECT } from '../_constants'
 
-export const addProject = (name, duration, leader, admin) => ({
+export const addProject = ({name, duration, leader, admin}) => ({
     type: ADD_PROJECT,
     name,
     duration,
