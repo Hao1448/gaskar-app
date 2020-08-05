f87344f8fe578651dbe49631125a8a48182ffb32
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-05 17:55:49 +0300

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import NavMenu from '../components/NavMenu'
 import  WidgetProjects  from '../components/WidgetProjects'
+import { ProfileImage } from '../components/ProfileImage'
+import { UiTabs } from '../components/Ui/UiTabs'
+import { NavLink } from 'react-router-dom'
 
 export const Projects = () => {
   return (
@@ -9,8 +12,17 @@ export const Projects = () => {
           <div className="projects_col-menu">
               <NavMenu/>
           </div>
-          <div className="projects_col-main">
-            <WidgetProjects/>
+            <div className="projects_col-main">
+              <div className="projects-header">
+                <NavLink to='/'>
+                  <ProfileImage/>
+                </NavLink>
+              </div>    
+              <div className="projects-title">
+                Проекты
+              </div>
+              <UiTabs/>
+              <WidgetProjects/>
           </div>
       </div> 
     </div>
