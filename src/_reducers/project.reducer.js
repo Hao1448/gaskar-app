668cd9bb7a88dc0eb21ef7056d705664c83c9ece
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-08-06 14:52:05 +0300

    Update project.reducer.js

--- src/_reducers/project.reducer.js
+++ src/_reducers/project.reducer.js
@@ -3,17 +3,19 @@ import { ADD_PROJECT } from '../_constants'
 
 const Projects = [
     {
+        id: 1,
         name: 'ТЕРАПЕВТИЧЕСКИЙ КОРПУС',
         duration: '08.04.2019-31.01.21 гг.',
         leader: 'Денис Конев',
         admin: 'Гросолим Лимитед'
     }
 ] 
-const projects = (state = Projects, { name, duration, leader, admin, type}) => {
+const projects = (state = Projects, { name, duration, leader, admin, type, id}) => {
     switch(type) {
         case ADD_PROJECT : 
         return [
             ...state, {
+                id,
                 name,
                 duration,
                 leader,
